d88fa7a9f7279623770ee531d5aa09864a1e83a2
Letcheverry <63750927+Letcheverry@users.noreply.github.com>
2022-11-14 12:41:09 -0300

    Initial commit

---
+++ 01-tipos-basicos.ts
@@ -0,0 +1,14 @@
+console.log('Hola mundo');
+
+
+// IMPORTANTE:
+// Ver la consola de JavaScript
+
+let nombre: string = 'Strider';
+let hp: number | string = 95;
+let estaVivo: boolean = true;
+
+
+hp = 'FULL';
+
+console.log(nombre, hp);
---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-qb7vsu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-qb7vsu)
